4e7a3eee856caed9423d8d688f92da8da72c2376
sinera2000 <wijethungawms.20@itfac.mrt.ac.lk>
2024-05-29 12:10:54 +0530

    Update from dev origin

--- update-database.sh
+++ update-database.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# Wait for the MySQL server to be available
+echo "Waiting for MySQL..."
+while ! nc -z mysql 3306; do   
+  sleep 1
+done
+echo "MySQL started"
 
-# Run database migration
+# Run EF Core database update
 dotnet ef database update --context HMSDBContext
 
 # Start the application
