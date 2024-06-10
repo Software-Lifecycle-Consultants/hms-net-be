b6fd4e8de91318016259a78a81130b5cfab348dd
Sanjeewa Lakmal <87803867+Sanjeewa001@users.noreply.github.com>
2024-06-11 00:03:33 +0530

    Implemented Model for contactUs details

---
+++ Models/Admin/AdminContact.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+    public class AdminContact
+    {
+
+        public String pageTitle { get; set; } 
+        public String pageDescription { get; set; }
+        public String telephonNumber { get; set; }
+        public String address { get; set; }
+        public String email { get; set; }
+    }
