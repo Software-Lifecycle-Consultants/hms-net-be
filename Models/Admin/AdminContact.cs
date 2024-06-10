dab9768914bcb1bac8ee9bc760b648e8ab15c353
chithraka99 <chithrakanayana99@gmail.com>
2024-06-10 20:05:26 +0530

    Implemented the Admin Contact Model

---
+++ Models/Admin/AdminContact.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HMS.Models.Admin
+{
+    public class AdminContact
+    {
+        public string PageTitle { get; set; } = string.Empty;
+        public string PageDescription { get; set; } = string.Empty;
+        public string TelephoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
