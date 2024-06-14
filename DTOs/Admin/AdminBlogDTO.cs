f1e963266c84daac93d08891a798cf36963553a9
sinera2000 <wijethungawms.20@itfac.mrt.ac.lk>
2024-06-14 05:40:28 +0530

    Create admin blog model and dto

---
+++ DTOs/Admin/AdminBlogDTO.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HMS.DTOs.Admin
+{
+    public class AdminBlogDTO
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "The Title must be between 10 and 100 characters.")]
+        public string Title { get; set; } = string.Empty;
+
+
+        [StringLength(150, MinimumLength = 10)]
+        public string? Subtitle { get; set; }
+
+        [NotMapped]
+        public IFormFile? CoverImage { get; set; }
+
+        [Required]
+        public DateTime PublishedTime { get; set; }
+
+
+        public List<string> Tags { get; set; } = [];
+
+        [Required]
+        [StringLength(10000, MinimumLength = 100)]
+        public string BlogContent { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string AuthorName { get; set; } = string.Empty;
+
+
+        [StringLength(500, MinimumLength = 10)]
+        public string? AuthorDescription { get; set; }
+
+        [NotMapped]
+        public IFormFile? AuthorImage { get; set; }
+
+
+        [Url]
+        public string? Facebook { get; set; }
+
+
+        [Url]
+        public string? Twitter { get; set; }
+
+
+        [Url]
+        public string? LinkedIn { get; set; }
+    }
+}
