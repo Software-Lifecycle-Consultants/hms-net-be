7a0c146c23cd10620f10b5babb3e42e7ca7811b7
sinera2000 <wijethungawms.20@itfac.mrt.ac.lk>
2024-07-11 08:07:04 +0530

    commit migration files

---
+++ HMS/Migrations/20240703144751_Admin-blog-update.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adminblogupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorImagePath",
+                table: "AdminBlogs",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImagePath",
+                table: "AdminBlogs",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorImagePath",
+                table: "AdminBlogs");
+
+            migrationBuilder.DropColumn(
+                name: "CoverImagePath",
+                table: "AdminBlogs");
+        }
+    }
+}
