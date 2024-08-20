d0562344c415e2da5398ce8a3dcd7afc3c234b4e
chithraka99 <chithrakanayana99@gmail.com>
2024-08-20 14:06:54 +0530

    AdminMealsAndServices Model

---
+++ HMS/Models/Admin/AdminMealsAndServices.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HMS.Models.Admin
+{
+    public class AdminMealsAndServices
+    {
+        [Required, Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public List<AdminMealsAndServicesValue>? AdminMealsAndServicesValue { get; set; }
+
+    }
+}
