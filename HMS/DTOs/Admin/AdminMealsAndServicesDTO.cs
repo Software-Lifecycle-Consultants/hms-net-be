805043809fb3b82f70832535ab23e0caf6c404bc
chithraka99 <chithrakanayana99@gmail.com>
2024-08-20 13:23:28 +0530

    Admin Meals And Services DTO

---
+++ HMS/DTOs/Admin/AdminMealsAndServicesDTO.cs
@@ -0,0 +1,12 @@
+﻿using HMS.Models.Admin;
+using System.ComponentModel.DataAnnotations;
+
+namespace HMS.DTOs.Admin
+{
+    public class AdminMealsAndServicesDTO
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public List<AdminMealsAndServicesValuesDTO>? AdminMealsAndServicesValues { get; set; }
+    }
+}
